f3cf47bb084eeea18a993bca84866a91912ec638
ms10-create <124409050+ms10-create@users.noreply.github.com>
2026-04-22 21:41:58 +0200

    Update app_lang.dart

--- lib/core/app_lang.dart
+++ lib/core/app_lang.dart
@@ -1,29 +1,40 @@
 import 'dart:convert';
 import 'package:flutter/services.dart';
-import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AppLang {
   static Map<String, String> _texts = {};
-  static String currentLang = 'English';
+  static Locale currentLocale = const Locale('en');
 
-  // ✅ أي صفحة تقدر تسمع وتعمل rebuild لما اللغة تتغير
-  static final ValueNotifier<int> notifier = ValueNotifier<int>(0);
+  static final ValueNotifier<Locale> notifier =
+      ValueNotifier(const Locale('en'));
 
-  // ✅ تحميل اللغة
-  static Future<void> load(String lang) async {
-    currentLang = lang;
-    final file = (lang == 'العربية')
+  // تحميل اللغة
+  static Future<void> load(String langCode) async {
+    currentLocale = Locale(langCode);
+
+    final file = (langCode == 'ar')
         ? 'assets/lang/ar.json'
         : 'assets/lang/en.json';
 
     final data = await rootBundle.loadString(file);
     _texts = Map<String, String>.from(json.decode(data));
 
-    // ✅ نعلن إن اللغة اتغيرت
-    notifier.value++;
+    // حفظ اللغة
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString("lang", langCode);
+
+    notifier.value = currentLocale;
   }
 
-  // ✅ الحصول على الترجمة
+  // تحميل اللغة المحفوظة
+  static Future<void> loadSaved() async {
+    final prefs = await SharedPreferences.getInstance();
+    final lang = prefs.getString("lang") ?? 'en';
+    await load(lang);
+  }
+
   static String t(String key) {
     return _texts[key] ?? key;
   }
