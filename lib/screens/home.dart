a5816ecf6274fa53519cb3d913aff754dab46602
ms10-create <124409050+ms10-create@users.noreply.github.com>
2026-05-15 15:59:18 +0300

    Update home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -257,7 +257,7 @@ class _HomeScreenState extends State<HomeScreen>
                           Navigator.push(
                             context,
                             MaterialPageRoute(
-                              builder: (_) => const NewContactPage(),
+                              builder: (_) => const EmergencyPage(),
                             ),
                           );
                         },
