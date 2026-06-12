ffc8d31bc54f6fa5e7124b6dc01facca16e51eb9
ms10-create <124409050+ms10-create@users.noreply.github.com>
2026-06-13 02:39:30 +0300

    Update avatar_screen.dart

--- lib/screens/avatar_screen.dart
+++ lib/screens/avatar_screen.dart
@@ -1,1 +1,103 @@
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class AvatarScreen extends StatefulWidget {
+  final List<dynamic> signs;
+
+  const AvatarScreen({
+    super.key,
+    required this.signs,
+  });
+
+  @override
+  State<AvatarScreen> createState() =>
+      _AvatarScreenState();
+}
+
+class _AvatarScreenState
+    extends State<AvatarScreen> {
+
+  late final WebViewController controller;
+
+  int currentSign = 0;
+  int currentFrame = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller =
+        WebViewController()
+          ..setJavaScriptMode(
+            JavaScriptMode.unrestricted,
+          )
+          ..loadFlutterAsset(
+            'assets/avatar_player.html',
+          );
+
+    Future.delayed(
+      const Duration(seconds: 2),
+      startAnimation,
+    );
+  }
+
+  Future<void> startAnimation() async {
+
+    while (mounted) {
+
+      if (widget.signs.isEmpty) {
+        await Future.delayed(
+          const Duration(milliseconds: 100),
+        );
+        continue;
+      }
+
+      final sign =
+          widget.signs[currentSign];
+
+      final frame =
+          sign.frames[currentFrame];
+
+      await controller.runJavaScript(
+        '''
+        animateFrame(
+          ${jsonEncode({
+            "leftHand": frame.leftHand,
+            "rightHand": frame.rightHand,
+          })}
+        );
+        ''',
+      );
+
+      await Future.delayed(
+        const Duration(milliseconds: 40),
+      );
+
+      currentFrame++;
+
+      if (currentFrame >= sign.frames.length) {
+
+        currentFrame = 0;
+
+        currentSign++;
+
+        if (currentSign >=
+            widget.signs.length) {
+          currentSign = 0;
+        }
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: WebViewWidget(
+        controller: controller,
+      ),
+    );
+  }
+}
