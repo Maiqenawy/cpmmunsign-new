cdb006344ca95fefedb3f88b851db8aee23ed8ed
ms10-create <124409050+ms10-create@users.noreply.github.com>
2026-06-16 17:45:27 +0300

    Update avatar_sign_model.dart

--- lib/screens/avatar_sign_model.dart
+++ lib/screens/avatar_sign_model.dart
@@ -1,32 +1,11 @@
-class AvatarFrame {
-  final List leftHand;
-  final List rightHand;
-
-  AvatarFrame({
-    required this.leftHand,
-    required this.rightHand,
-  });
-
-  factory AvatarFrame.fromJson(
-    Map<String, dynamic> json,
-  ) {
-    return AvatarFrame(
-      leftHand: json["left_hand"],
-      rightHand: json["right_hand"],
-    );
-  }
-}
-
 class AvatarSign {
   final String word;
   final int frames;
-  final int landmarksPerFrame;
-  final List<dynamic> landmarks;
+  final List<List<double>> landmarks;
 
   AvatarSign({
     required this.word,
     required this.frames,
-    required this.landmarksPerFrame,
     required this.landmarks,
   });
 
@@ -35,9 +14,15 @@ class AvatarSign {
   ) {
     return AvatarSign(
       word: json["word"],
+
       frames: json["frames"],
-      landmarksPerFrame: json["landmarks_per_frame"],
-      landmarks: json["landmarks"],
+
+      landmarks:
+          (json["landmarks"] as List)
+              .map<List<double>>(
+                (e) => List<double>.from(e),
+              )
+              .toList(),
     );
   }
 }
