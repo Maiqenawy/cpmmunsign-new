a1967a3d2be8e31d59296051aae7b90c5ebc47cb
ms10-create <124409050+ms10-create@users.noreply.github.com>
2026-06-16 17:58:30 +0300

    Update avatar_screen.dart

--- lib/screens/avatar_screen.dart
+++ lib/screens/avatar_screen.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class AvatarScreen extends StatefulWidget {
-  final List<dynamic> signs;
+  final List<AvatarSign> signs;
 
   const AvatarScreen({
     super.key,
